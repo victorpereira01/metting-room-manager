54c8f681ea9c3855d7f06f73e83dbc9e8eba7dc8
Victor Pereira <victor.cordeiro2@gmail.com>
2020-09-25 22:49:19 -0300

    build: server initial setup

---
+++ backend/src/main/java/com/victorpereira/mrmanager/MettingRoomManagerApplication.java
@@ -0,0 +1,13 @@
+package com.victorpereira.mrmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MettingRoomManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MettingRoomManagerApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/victorpereira/mrmanager/MettingRoomManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victorpereira.mrmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MettingRoomManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
