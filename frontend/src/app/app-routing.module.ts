286e91de73567db424b668454ebab7d4b3913067
Victor Pereira <victor.cordeiro2@gmail.com>
2020-09-30 16:00:19 -0300

    feat: service and routing config

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CreateRoomComponent } from './components/create-room/create-room.component';
+import { RoomDetailsComponent } from './components/room-details/room-details.component';
+import { RoomListComponent } from './components/room-list/room-list.component';
+import { UpdateRoomComponent } from './components/update-room/update-room.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'rooms', pathMatch: 'full' },
+  { path: 'rooms', component: RoomListComponent },
+  { path: 'add', component: CreateRoomComponent },
+  { path: 'update/:id', component: UpdateRoomComponent },
+  { path: 'details/:id', component: RoomDetailsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CreateRoomComponent } from './create-room/create-room.component';
-import { RoomDetailsComponent } from './room-details/room-details.component';
-import { RoomListComponent } from './room-list/room-list.component';
-import { UpdateRoomComponent } from './update-room/update-room.component';
+import { CreateRoomComponent } from './components/create-room/create-room.component';
+import { RoomDetailsComponent } from './components/room-details/room-details.component';
+import { RoomListComponent } from './components/room-list/room-list.component';
+import { UpdateRoomComponent } from './components/update-room/update-room.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { UpdateRoomComponent } from './update-room/update-room.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
